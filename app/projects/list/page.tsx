04f501c924d366e7e4711f0c21c9b42747498d31
bsegers <briansegs@gmail.com>
2024-04-28 16:53:04 -0400

    feat: Add dynamic routes

--- app/projects/list/page.tsx
+++ app/projects/list/page.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
+import styles from "./projects.module.css";
+
 export default function Home() {
   return (
     <main>
       <h1>ProjectsList</h1>
+
+      <ul className={styles.ul}>
+        <li>
+          <Link href="/projects/jobit">Jobit</Link>
+        </li>
+        <li>
+          <Link href="/projects/carrent">Car Rent</Link>
+        </li>
+        <li>
+          <Link href="/projects/hipnode">hipnode</Link>
+        </li>
+      </ul>
     </main>
   );
 }
